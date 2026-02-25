af63c8ae5c857e7ca23f7acd48493e152622275e
Thang <thang.edreams@gmail.com>
2026-02-25 13:24:00 +0100

    fix: load node in xcode cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,8 +2,20 @@
 set -e
 set -x
 
-echo "📦 Go to repo root"
-cd "$(git rev-parse --show-toplevel)"
+# aller à la racine repo
+REPO_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
+cd "$REPO_ROOT"
+
+echo "📦 Load NVM"
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
+echo "📦 Use Node 18"
+nvm install 18
+nvm use 18
+
+node -v
+npm -v
 
 echo "📦 Install JS deps"
 npm install
