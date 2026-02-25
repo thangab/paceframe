5eb73a276a03f0c1a6e43e9af3446aa6704dbd2d
Thang <thang.edreams@gmail.com>
2026-02-25 13:32:18 +0100

    fix: node via brew for xcode cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,17 +2,12 @@
 set -e
 set -x
 
-# aller à la racine repo
+# repo root
 REPO_ROOT="$(cd "$(dirname "$0")/../.." && pwd)"
 cd "$REPO_ROOT"
 
-echo "📦 Load NVM"
-export NVM_DIR="$HOME/.nvm"
-source "$NVM_DIR/nvm.sh"
-
-echo "📦 Use Node 18"
-nvm install 18
-nvm use 18
+echo "📦 Install Node via Homebrew"
+brew install node
 
 node -v
 npm -v
