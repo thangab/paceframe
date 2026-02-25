0ad01829c44b1fdabe154fbbd67b7c8d7bc24058
Thang <thang.edreams@gmail.com>
2026-02-25 13:10:16 +0100

    fix: ci script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
+set -x
 
-echo "📦 Installing node modules"
+echo "📦 Setup Node"
+if command -v node >/dev/null 2>&1; then
+  echo "Node exists"
+else
+  brew install node
+fi
+
+echo "📦 Install JS deps"
 npm install
 
-echo "📦 Installing CocoaPods"
+echo "📦 Install CocoaPods"
 cd ios
 pod install --repo-update
